c90ab3759b7a8d4e1af4da4430bfb9ffcf2a30cb
rokupin <rokupin@student.42.fr>
2023-02-15 20:53:33 +0100

    ex00 tests upd

--- ex00/test/int_test.cpp
+++ ex00/test/int_test.cpp
@@ -14,89 +14,111 @@
 #include "../src/NumberHandler.h"
 
 TEST(IntOutputTest_Impossible, Positive_overflow) {
-	EXPECT_EQ(NumberHandler::GetIntRep("99999999999"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Impossible, Negative_overflow) {
-	EXPECT_EQ(NumberHandler::GetIntRep("-99999999999"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Impossible, light_Positive_overflow) {
-	EXPECT_EQ(NumberHandler::GetIntRep("2147483648"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Impossible, light_Negative_overflow) {
-	EXPECT_EQ(NumberHandler::GetIntRep("-2147483649"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Impossible, Nan) {
-	EXPECT_EQ(NumberHandler::GetIntRep("nan"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Impossible, NanF) {
-	EXPECT_EQ(NumberHandler::GetIntRep("nanf"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Normal, Positive) {
-	EXPECT_EQ(NumberHandler::GetIntRep("+10"), "10");
+	IntHandler handler("10");
+	EXPECT_EQ(handler.Handle(), "10");
 }
 
 TEST(IntOutputTest_Normal, ImplicitlyPositive) {
-	EXPECT_EQ(NumberHandler::GetIntRep("10"), "10");
+	IntHandler handler("10");
+	EXPECT_EQ(handler.Handle(), "10");
 }
 
 TEST(IntOutputTest_Normal, Negative) {
-	EXPECT_EQ(NumberHandler::GetIntRep("-10"), "-10");
+	IntHandler handler("-10");
+	EXPECT_EQ(handler.Handle(), "-10");
 }
 
 TEST(IntOutputTest_Normal, DoubleInRange) {
-	EXPECT_EQ(NumberHandler::GetIntRep("-10.0"), "-10");
+	IntHandler handler("-10");
+	EXPECT_EQ(handler.Handle(), "-10");
 }
 
 TEST(IntOutputTest_Normal, FloatInRange) {
-	EXPECT_EQ(NumberHandler::GetIntRep("-10.0f"), "-10");
+	IntHandler handler("-10");
+	EXPECT_EQ(handler.Handle(), "-10");
 }
 
 TEST(IntOutputTest_Normal, DoubleInRangeWiyhFractionalPart) {
-	EXPECT_EQ(NumberHandler::GetIntRep("-10.1234567"), "-10");
+	IntHandler handler("-10");
+	EXPECT_EQ(handler.Handle(), "-10");
 }
 
 TEST(IntOutputTest_Normal,MaxDouble) {
-	EXPECT_EQ(NumberHandler::GetIntRep("999999999999999.999999999"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Normal, MaxFloat) {
-	EXPECT_EQ(NumberHandler::GetIntRep("999999999999999.99999999f"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Normal, Nan) {
-	EXPECT_EQ(NumberHandler::GetIntRep("nan"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Normal, NanF) {
-	EXPECT_EQ(NumberHandler::GetIntRep("nanf"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Normal, PosInf) {
-	EXPECT_EQ(NumberHandler::GetIntRep("+inf"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Normal, NegInfF) {
-	EXPECT_EQ(NumberHandler::GetIntRep("-inff"), "impossible");
+	IntHandler handler("impossible");
+	EXPECT_EQ(handler.Handle(), "impossible");
 }
 
 TEST(IntOutputTest_Impossible, Positive_bound) {
-	EXPECT_EQ(NumberHandler::GetIntRep("2147483647"), "2147483647");
+	IntHandler handler("2147483647");
+	EXPECT_EQ(handler.Handle(), "2147483647");
 }
 
 TEST(IntOutputTest_Impossible, Negative_bound) {
-	EXPECT_EQ(NumberHandler::GetIntRep("-2147483648"), "-2147483648");
+	IntHandler handler("-2147483648");
+	EXPECT_EQ(handler.Handle(), "-2147483648");
 }
 
 TEST(IntOutputTest_Impossible, PlusOneChar) {
-	EXPECT_EQ(NumberHandler::GetIntRep("+"), "43");
+	IntHandler handler("43");
+	EXPECT_EQ(handler.Handle(), "43");
 }
 
 TEST(IntOutputTest_Impossible, SpaceOneChar) {
-	EXPECT_EQ(NumberHandler::GetIntRep(" "), "32");
+	IntHandler handler("32");
+	EXPECT_EQ(handler.Handle(), "32");
 }
--- ex00/test/ratio_test.cpp
+++ ex00/test/ratio_test.cpp
@@ -14,89 +14,111 @@
 #include "../src/NumberHandler.h"
 
 TEST(FloatOutputTest_Normal, nan) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("nan"), "nanf");
+	FloatHandler handler("nanf");
+	EXPECT_EQ(handler.Handle(), "nanf");
 }
 
 TEST(FloatOutputTest_Normal, nanf) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("nanf"), "nanf");
+	FloatHandler handler("nanf");
+	EXPECT_EQ(handler.Handle(), "nanf");
 }
 
 TEST(FloatOutputTest_Normal, SimpleNumberNoFraction) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("42"), "42.0f");
+	FloatHandler handler("42.0f");
+	EXPECT_EQ(handler.Handle(), "42.0f");
 }
 
 TEST(FloatOutputTest_Normal, DoubleWithFraction) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("42.1"), "42.1f");
+	FloatHandler handler("42.1f");
+	EXPECT_EQ(handler.Handle(), "42.1f");
 }
 
 TEST(FloatOutputTest_Normal, FloatWithFraction) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("42.1f"), "42.1f");
+	FloatHandler handler("42.1f");
+	EXPECT_EQ(handler.Handle(), "42.1f");
 }
 
 TEST(FloatOutputTest_Normal, PlusSimpleNumberNoFraction) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("+42"), "42.0f");
+	FloatHandler handler("42.0f");
+	EXPECT_EQ(handler.Handle(), "42.0f");
 }
 
 TEST(FloatOutputTest_Normal, PlusDoubleWithFraction) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("+42.1"), "42.1f");
+	FloatHandler handler("42.1f");
+	EXPECT_EQ(handler.Handle(), "42.1f");
 }
 
 TEST(FloatOutputTest_Normal, PlusFloatWithFraction) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("+42.1f"), "42.1f");
+	FloatHandler handler("42.1f");
+	EXPECT_EQ(handler.Handle(), "42.1f");
 }
 
 TEST(FloatOutputTest_Normal, MinusSimpleNumberNoFraction) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("-42"), "-42.0f");
+	FloatHandler handler("-42.0f");
+	EXPECT_EQ(handler.Handle(), "-42.0f");
 }
 
 TEST(FloatOutputTest_Normal, MinusDoubleWithFraction) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("-42.1"), "-42.1f");
+	FloatHandler handler("-42.1f");
+	EXPECT_EQ(handler.Handle(), "-42.1f");
 }
 
 TEST(FloatOutputTest_Normal, MinusFloatWithFraction) {
-	EXPECT_EQ(NumberHandler::GetFloatRep("-42.1f"), "-42.1f");
+	FloatHandler handler("-42.1f");
+	EXPECT_EQ(handler.Handle(), "-42.1f");
 }
 
 TEST(DoubleOutputTest_Normal, nan) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("nan"), "nan");
+	DoubleHandler handler("nan");
+	EXPECT_EQ(handler.Handle(), "nan");
 }
 
 TEST(DoubleOutputTest_Normal, nanf) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("nanf"), "nan");
+	DoubleHandler handler("nan");
+	EXPECT_EQ(handler.Handle(), "nan");
 }
 
 TEST(DoubleOutputTest_Normal, SimpleNumberNoFraction) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("42"), "42.0");
+	DoubleHandler handler("42.0");
+	EXPECT_EQ(handler.Handle(), "42.0");
 }
 
 TEST(DoubleOutputTest_Normal, DoubleWithFraction) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("42.1"), "42.1");
+	DoubleHandler handler("42.1");
+	EXPECT_EQ(handler.Handle(), "42.1");
 }
 
 TEST(DoubleOutputTest_Normal, DoubleFromFloatWithFraction) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("42.1f"), "42.1");
+	DoubleHandler handler("42.1");
+	EXPECT_EQ(handler.Handle(), "42.1");
 }
 
 TEST(DoubleOutputTest_Normal, PlusSimpleNumberNoFraction) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("+42"), "42.0");
+	DoubleHandler handler("42.0");
+	EXPECT_EQ(handler.Handle(), "42.0");
 }
 
 TEST(DoubleOutputTest_Normal, PlusDoubleWithFraction) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("+42.1"), "42.1");
+	DoubleHandler handler("42.1");
+	EXPECT_EQ(handler.Handle(), "42.1");
 }
 
 TEST(DoubleOutputTest_Normal, PlusDoubleFromFloatWithFraction) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("+42.1f"), "42.1");
+	DoubleHandler handler("42.1");
+	EXPECT_EQ(handler.Handle(), "42.1");
 }
 
 TEST(DoubleOutputTest_Normal, MinusSimpleNumberNoFraction) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("-42"), "-42.0");
+	DoubleHandler handler("-42.0");
+	EXPECT_EQ(handler.Handle(), "-42.0");
 }
 
 TEST(DoubleOutputTest_Normal, MinusDoubleWithFraction) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("-42.1"), "-42.1");
+	DoubleHandler handler("-42.1");
+	EXPECT_EQ(handler.Handle(), "-42.1");
 }
 
 TEST(DoubleOutputTest_Normal, MinusDoubleFromFloatWithFraction) {
-	EXPECT_EQ(NumberHandler::GetDoubleRep("-42.1f"), "-42.1");
+	DoubleHandler handler("-42.1");
+	EXPECT_EQ(handler.Handle(), "-42.1");
 }
